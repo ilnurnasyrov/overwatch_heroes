ba5eb569b83dcfab6f2b7abbbca22b72551d3333
Ilnur Nasyrov <ilnur.nasyrov.92@gmail.com>
2019-03-05 04:32:59 +0300

    Move vcr cassettes to spec folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -105,6 +105,6 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = File.expand_path("cassettes", __dir__)
   config.hook_into :webmock
 end
